61f7034c2d345d2a9b7b7ceb0d66c742951151b9
jamieA9 <jamieaw925@gmail.com>
2023-03-29 14:50:33 -0400

    help

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-// test48765
 // this is our login page 
 import logo from './logo.svg';
 import './App.css';
 
+import Headr from "./components/Headr.js";
+import LogPage from "./Login";
+
+
+//this is where everything is displayed so we call other classes -- basically a main
 function App() {
   return (
-    <div className="App-header">
-     Welcome to Hogwarts University
+    <div className="App">
+     <LogPage />
     </div>
+
   );
 }
+
 
 export default App;
