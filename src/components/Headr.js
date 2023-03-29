e74d5721a89890b3b07a908d0adbaed77d11df1f
jamieA9 <jamieaw925@gmail.com>
2023-03-29 14:47:00 -0400

    updates

---
+++ src/components/Headr.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Headr.css";
+
+//in the components folder im putting stuff were gonna use on every page
+//footer, header, signout ect
+function headerNav() {
+  return (
+    <div className='header'>Welcome to Hogwarts University </div>
+  )
+}
+
+export default headerNav
