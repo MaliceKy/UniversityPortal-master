698601649e5e0d1aa77b2e9d76689ed9db84bdc1
MaliceKy <Kyle.Malice2001@gmail.com>
2023-03-29 14:26:46 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*to get this to work you need to download npm for your computer.
+he gave us a link in the emails he sent us 
+i recommend downlaoding from the repository provided in the group chat, 
+once you get the project put up to be able to see the actual webpage u need to run npm
+im not sure how you would do it in any other IDE but in VSC
+u can pull up terminal at the bottom and type in npm start and 
+it should pull a webpage that sayd Welcome to HU -- which is all we have at the moment
+if it doesnt work lmk i had to do a bunch of stuff to actually get it working but since
+im sharing it im not sure if you guys need to or not */
+
+// this is our login page 
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+     Welcome to Hogwarts University
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headr from "./components/Headr.js";
+
+function LogPage() {
+    return (
+      <div className="head">
+       <Headr />
+      </div>
+  
+    );
+  }
+
+export default LogPage
