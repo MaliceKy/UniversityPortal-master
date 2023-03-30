2e8b97254ec631d872e568717946e597bfc25423
MaliceKy <Kyle.Malice2001@gmail.com>
2023-03-30 16:42:37 -0400

    added logo to banner, alonmg with images file.

--- src/components/Headr.js
+++ src/components/Headr.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "../styles/Headr.css";
+import logo from '../images/HogLogo.png';
 
 //in the components folder im putting stuff were gonna use on every page
 //footer, header, signout ect
 function headerNav() {
   return (
-    <div className='header'>Welcome to Hogwarts University</div>
+    <div className='header'>
+      Welcome to Hogwarts University
+      <img src={logo} alt="Logo" className="logo" />
+    </div>
   )
 }
 
