b0e703cb89af7e73cd5a83361f0c9e5af79512d8
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-04-18 13:04:49 -0400

    Fixed up the profile page so ity displays

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Login from './pages/Login';
 import Pages from './pages/Course';
 import School from './pages/School';
 import Header from './components/Headr';
+import Profile from './pages/Profile';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('school');
   const [userRole, setUserRole] = useState('');
+  const [userId, setUserId] = useState(null);
 
   const handleLoginSuccess = (user) => {
     setUserRole(user.role);
+    setUserId(user.ID);
     setCurrentPage('courses');
   };
 
@@ -22,6 +25,8 @@ function App() {
     content = <Login onLoginSuccess={handleLoginSuccess} />;
   } else if (currentPage === 'courses') {
     content = <Pages setCurrentPage={setCurrentPage} userRole={userRole} />;
+  } else if (currentPage === 'profile') {
+    content = <Profile currentUser={userId} />;
   }
 
   return (
