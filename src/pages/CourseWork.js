641c262e91bb9ebb3d0923d36caaa14b4ec0b9ef
jamieA9 <jamieaw925@gmail.com>
2023-04-19 19:15:43 -0400

    updated json file -- error in profile

---
+++ src/pages/CourseWork.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CourseWork() {
+  return (
+    <div>CourseWork</div>
+  )
+}
+
+export default CourseWork
--- src/pages/User.js
+++
@@ -1,5 +0,0 @@
-/* this is going to be a parentclass for student and teacher,
-should have basic instuctions like firstname lastname ect.*/
-
-import React from 'react'; 
-
