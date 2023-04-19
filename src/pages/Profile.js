d53ff5d017923784779e9f7c9dc56a75618b30e5
jamieA9 <jamieaw925@gmail.com>
2023-04-19 19:14:40 -0400

    Added to json file -- have error in profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import LoginData from '../data/login.json';
 import '../styles/Profile.css';
+import CourseData from '../data/courses.json';
+
+
+
 
 function Profile({ currentUser }) {
+  const [courseName, setCourseName] = useState('');
+
   const user = LoginData.find(user => user.ID === currentUser);
+  const class1 = CourseData.find(class1 => class1.studentsEnrolledArray === user.ID );
+
+  setCourseName(class1.courseName);
 
   return (
     <div className="profile-container">
@@ -12,6 +21,8 @@ function Profile({ currentUser }) {
         <p>Name: {user.name}</p>
         <p>Email: {user.email}</p>
         <p>Role: {user.role}</p>
+        <p>Your Classes: {class1.courseName}</p>
+        
       </div>
     </div>
   );
