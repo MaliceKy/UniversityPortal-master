6a202209195f381a9e36d818f195c6ea3d7065b5
MaliceKy <129139550+MaliceKy@users.noreply.github.com>
2023-04-21 18:30:35 -0400

    Gitlog bug fixed buttons now

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,20 +2,10 @@
 import { useState, useEffect } from 'react';
 import NavigationButtons from '../components/NavigationButtons';
 import LoginData from '../data/login.json';
-<<<<<<< HEAD
-=======
-import '../styles/Profile.css';
-import '../styles/Course.css';
->>>>>>> 2c98fae86cbee3feb2229f28267809476478d1ec
 import CourseData from '../data/courses.json';
 import '../styles/Profile.css';
 
-<<<<<<< HEAD
 function Profile({ currentUser, setCurrentPage }) {
-=======
-
-function Profile({ currentUser }) {
->>>>>>> 2c98fae86cbee3feb2229f28267809476478d1ec
   const [courseName, setCourseName] = useState('');
 
   const user = LoginData.find(user => user.ID === currentUser);
@@ -27,7 +17,6 @@ function Profile({ currentUser }) {
   }, [currentUser, user]);
 
   return (
-<<<<<<< HEAD
     <div>
       <NavigationButtons setCurrentPage={setCurrentPage} />
       <div className="profile-wrapper">
@@ -40,19 +29,7 @@ function Profile({ currentUser }) {
             <p> Your Classes: {courseName} </p>
           </div>
         </div>
-=======
-    <div >
-      
-    <div className="profile-container">
-      <h2>Profile</h2>
-      <div className="profile-info">
-        <p>Name: {user.name}</p>
-        <p>Email: {user.email}</p>
-        <p>Role: {user.role}</p>
-        <p> Your Classes: {courseName} </p>
->>>>>>> 2c98fae86cbee3feb2229f28267809476478d1ec
       </div>
-    </div>
     </div>
   );
 }
