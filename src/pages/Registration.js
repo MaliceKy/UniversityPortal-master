81b8b4e9bcea5d466fe166018e38b354823a8a71
princeuu <princeyclarky@gmail.com>
2023-04-22 02:55:24 -0400

    nothing

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import "../styles/Registration.css";
+import NavigationButtons from '../components/NavigationButtons';
 
-function Registration() {
+
+function Registration({ currentUser, setCurrentPage, userId }) {
+
     const courses = [
-        { name: 'Register for Dark Arts', description: 'Dark Arts', description2: 'Teacher: Dr. Snape - Schedule: MWF'},
-        { name: 'Register for Charms', description: 'Charms', description2: 'Teacher: Prof. Flitwick - Schedule: TTH'},
-        { name: 'Register for Potions ', description: 'Potions', description2: 'Teacher: Prof. Slughorn - Schedule: MWF'},
-        { name: 'Register for Xylomancy', description: 'Xylomancy', description2: 'Teacher: Prof. Trelawney - Schedule: TTH'},
+        { button: 'Register for Dark Arts', course: 'Dark Arts', description1: 'Teacher: Dr. Snape - Schedule: MWF', courseID: 106},
+        { button: 'Register for Charms', course: 'Charms', description1: 'Teacher: Prof. Flitwick - Schedule: TTH', courseID: 108},
+        { button: 'Register for Potions ', course: 'Potions', description1: 'Teacher: Prof. Slughorn - Schedule: MWF', courseID: 107},
+        { button: 'Register for Xylomancy', course: 'Xylomancy', description1: 'Teacher: Prof. Trelawney - Schedule: TTH',courseID: 109},
     ];
 
     const handleCourseClick = (event) => {
-        console.log('Course clicked:', event.target.innerText);
-        // Add your code to handle the course click event here
+      
     };
 
     const courseButtons = courses.map((course) => (
-      <div key={course.name} className="course-cardReg">
-        <p>{course.description}</p>
-        <p className="description2">{course.description2}</p>
+      
+      <div key={course.button} className="course-cardReg">
+        <p>{course.course}</p>
+        <p className="description1">{course.description1}</p>
         <button 
           className="course-button" 
           onClick={handleCourseClick}
-          key={course.name}
+          key={course.button}
         >
-          {course.name}
+          {course.button}
         </button>
       </div>
     ));
 
     return (
-        <div className="registration-text"style={{marginTop: '40px'}}>
-            <h2>Register for classes!</h2>
-            <div className="course-list" style={{marginTop: '100px'}}>
-                {courseButtons}
-            </div>
+      <div>
+      <NavigationButtons setCurrentPage={setCurrentPage} />
+      <div className="registration-text" style={{ marginTop: '40px' }}>
+        <h2>Register for classes!</h2>
+        <div className="course-list" style={{ marginTop: '100px' }}>
+          {courseButtons}
         </div>
-    )
+      </div>
+    </div>
+    );
 }
 
 export default Registration;
