877bc6550c2749e1267f215997ed0146cd749f33
jamieA9 <jamieaw925@gmail.com>
2023-04-24 10:09:28 -0400

    accessability

---
+++ src/pages/Accessibility.js
@@ -0,0 +1,53 @@
+
+
+import React from 'react';
+import '../styles/Accessibility.css';
+
+
+
+function Accessibility({userId, userRole, setCurrentPage }) {
+
+  return (
+
+    <div id = "access-container">
+        
+      <div >
+
+        Would you like to Enter a Class?
+        <h2>Complete the following</h2>
+        
+        <form id = "courseForm">
+        
+        <label for='courseName' class="form-label" id = "nam">Course Name:</label>
+        <input type="text" id='courseName' name="courseName" placeholder="Course Name" />
+   
+     
+        <label for='CourseID' class="form-label"> Course ID</label>
+        <input type="text" id='CourseID' name="CourseID" placeholder="Courses ID" />
+        
+        <label for='teachersID' class="form-label"> Teachers ID</label>
+        <input type="text" id='teachersID' name="teachersID" placeholder="Teachers ID" />
+        
+        <label for='teachersName' class="form-label">Your Name</label>
+        <input type="text" id='teachersName' name="teachersName" placeholder="Teachers Name" />
+    
+        <label for="description" class="form-label">Description</label>
+        <textarea type="text" id='description' name="description" placeholder="description" />
+
+        <label for='classRoom' class="form-label">Classroom number</label>
+        <input type="text" id='classRoom' name="classRoom" placeholder="Description" />
+
+        <label for='classTime ' class="form-label">Class Time</label>
+        <input type="text" id='classTime' name="classTime" placeholder="ClassTime" />
+        
+        <button type="submitbutton" id="submitAssignment">Submit</button>
+        
+  </form>
+      </div>
+      
+     
+    </div>
+  );
+}
+
+export default Accessibility
