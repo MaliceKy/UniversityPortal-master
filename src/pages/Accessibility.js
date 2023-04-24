c75d5457547aa85a3207d1283cb81bc09640592d
jamieA9 <jamieaw925@gmail.com>
2023-04-24 13:24:54 -0400

    fix

--- src/pages/Accessibility.js
+++ src/pages/Accessibility.js
@@ -18,26 +18,26 @@ function Accessibility({userId, userRole, setCurrentPage }) {
         
         <form id = "courseForm">
         
-        <label for='courseName' class="form-label" id = "nam">Course Name:</label>
+        <label for='courseName' class="form-label1" id = "nam">Course Name:</label>
         <input type="text" id='courseName' name="courseName" placeholder="Course Name" />
    
      
-        <label for='CourseID' class="form-label"> Course ID</label>
+        <label for='CourseID' class="form-label1"> Course ID</label>
         <input type="text" id='CourseID' name="CourseID" placeholder="Courses ID" />
         
-        <label for='teachersID' class="form-label"> Teachers ID</label>
+        <label for='teachersID' class="form-label1"> Teachers ID</label>
         <input type="text" id='teachersID' name="teachersID" placeholder="Teachers ID" />
         
-        <label for='teachersName' class="form-label">Your Name</label>
+        <label for='teachersName' class="form-label1">Your Name</label>
         <input type="text" id='teachersName' name="teachersName" placeholder="Teachers Name" />
     
-        <label for="description" class="form-label">Description</label>
+        <label for="description" class="form-label1">Description</label>
         <textarea type="text" id='description' name="description" placeholder="description" />
 
-        <label for='classRoom' class="form-label">Classroom number</label>
+        <label for='classRoom' class="form-label1">Classroom number</label>
         <input type="text" id='classRoom' name="classRoom" placeholder="Description" />
 
-        <label for='classTime ' class="form-label">Class Time</label>
+        <label for='classTime ' class="form-label1">Class Time</label>
         <input type="text" id='classTime' name="classTime" placeholder="ClassTime" />
         
         <button type="submitbutton" id="submitAssignment">Submit</button>
