a0f630d6366d5f9eb50120e9eab280c0287867c7
jamieA9 <jamieaw925@gmail.com>
2023-04-24 08:44:07 -0400

    need grades fixed -- student it fine teacher doesnt work

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -3,15 +3,19 @@ import NavigationButtons from '../components/NavigationButtons';
 import '../styles/Course.css';
 import CourseData from '../data/courses.json';
 import UserData from '../data/login.json';
+import GradeData from '../data/grades.json';
 
 function Course({ setCurrentPage, userRole, userId }) {
   const [courses, setCourses] = useState([]);
+  const [grades, setGrades] = useState([]);
 
   useEffect(() => {
     if (userRole === 'student') {
       const enrolledCourses = CourseData.filter(course =>
         course.studentsEnrolledArray.includes(userId)
       );
+      const studentGrades = GradeData.filter(grade => grade.studentID === userId);
+      setGrades(studentGrades);
       setCourses(enrolledCourses);
     } else if (userRole === 'teacher') {
       const teachingCourses = CourseData.filter(course => course.teachersID === userId);
@@ -19,6 +23,33 @@ function Course({ setCurrentPage, userRole, userId }) {
     }
   }, [userRole, userId]);
 
+  const getGrade = (courseId, teachersId) => {
+    if (userRole === 'student') {
+      const grade = grades.find(grade => grade.courseID === courseId && grade.studentID === userId);
+      if (grade) {
+        return <p>Grade: {grade.grade}</p>;
+      } else {
+        return <p>Grade not in yet</p>;
+      }
+    } else if (userRole === 'teacher' && teachersId === userId) {
+      const courseGrades = grades.filter(grade => grade.courseID === courseId);
+      if (courseGrades.length > 0) {
+        return (
+          <div>
+            <h4>Grades:</h4>
+            <ul>
+              {courseGrades.map(grade => (
+                <li key={grade.studentID}>{grade.studentName}: {grade.grade}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      } else {
+        return <p>No grades yet</p>;
+      }
+    }
+  
+};
   const getTeacherName = (teacherId) => {
     const teacher = UserData.find(user => user.ID === teacherId && user.role === 'teacher');
     return teacher ? teacher.name : 'Unknown';
@@ -44,6 +75,7 @@ function Course({ setCurrentPage, userRole, userId }) {
                 <hr />
                 <p>{course.classDays}</p>
                 <p>{course.classTime}</p>
+                {getGrade(course.courseID)}
               </div>
             ))}
           </div>
@@ -60,5 +92,4 @@ function Course({ setCurrentPage, userRole, userId }) {
 }
 
 export default Course;
-
 
